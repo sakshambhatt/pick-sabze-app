d092d127a28f3836fcd6daa6f1caa90a211d45b5
Saksham Bhatt <sakshambhatt33@gmail.com>
2022-01-24 00:07:16 +0530

    refactor: tailwind setup, meta content changed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
